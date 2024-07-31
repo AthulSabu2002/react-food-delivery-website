8aea3f594d05a65f3f246c476f8a73c51f63320e
athulsabu <athulsabu75@gmail.com>
2024-07-31 19:49:00 +0530

    added cart function

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,11 +1,44 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from "react";
 import { createContext } from "react";
-import { food_list } from "../assets/assets"; // Ensure this path is correct
+import PropTypes from "prop-types";
+import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+        } else {
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+        }
+    };
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => {
+            const newCount = prev[itemId] - 1;
+            if (newCount <= 0) {
+                // eslint-disable-next-line no-unused-vars
+                const { [itemId]: _, ...rest } = prev;
+                return rest;
+            }
+            return { ...prev, [itemId]: newCount };
+        });
+    };
+
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems]);
+
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart,
     };
 
     return (
@@ -13,6 +46,10 @@ const StoreContextProvider = (props) => {
             {props.children}
         </StoreContext.Provider>
     );
-}
+};
+
+StoreContextProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
 
 export default StoreContextProvider;
