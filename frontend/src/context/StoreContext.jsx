5809ab053282681844aae397194ea0afa48514c9
athulsabu <athulsabu75@gmail.com>
2024-08-10 22:52:45 +0530

    updated cart page

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -26,15 +26,28 @@ const StoreContextProvider = ({ children }) => {
         });
     };
 
-    useEffect(() => {
-        console.log("cartItems:", cartItems);
-    }, [cartItems]);
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+
+        for(const item in cartItems){
+            if(cartItems[item] > 0){
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount = totalAmount + (itemInfo.price * cartItems[item]);
+            }
+        }
+
+        return totalAmount;
+
+    }
+
 
     const contextValue = {
         food_list,
         cartItems,
         addToCart,
         removeFromCart,
+        getTotalCartAmount
     };
 
     return (
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -3,10 +3,13 @@
 import React, { useContext } from 'react'
 import './Cart.css'
 import {StoreContext} from '../../context/StoreContext'
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
 
-  const {cartItems, food_list, removeFromCart} = useContext(StoreContext);
+  const navigate = useNavigate();
+
+  const {cartItems, food_list, removeFromCart, getTotalCartAmount} = useContext(StoreContext);
 
   return (
     <div className='cart'>
@@ -26,19 +29,55 @@ const Cart = () => {
         {food_list.map((item, index)=>{
             if(cartItems[item._id]>0){
                 return(
-                  <div className="cart-items-title cart-items-item">
-                      <img src={item.image} alt="" />
-                      <p>{item.name}</p>
-                      <p>{item.price}</p>
-                      <p>{cartItems[item.id]}</p>
-                      <p>{item.price * cartItems[item.id]}</p>
-                      <p>X</p>
+                  <div>
+                      <div className="cart-items-title cart-items-item">
+                        <img src={item.image} alt="" />
+                        <p>{item.name}</p>
+                        <p>${item.price}</p>
+                        <p>{cartItems[item._id]}</p>
+                        <p>${item.price * cartItems[item._id]}</p>
+                        <p className='cross' onClick={() => removeFromCart(item._id)}>X</p>
+                    </div>
+                    <hr />
                   </div>
                 )
             }
         })}
 
       </div>
+
+        <div className="cart-bottom">
+          <div className="cart-total">
+            <h2>Cart Totals</h2>
+            <div>
+              <div className="cart-total-details">
+                <p>Subtotal</p>
+                <p>${getTotalCartAmount()}</p>
+              </div>
+              <div className="cart-total-details">
+                <p>Delivery Fee</p>
+                <p>${2}</p>
+              </div>
+              <div className="cart-total-details">
+                <b>Total</b>
+                <b>${getTotalCartAmount() + 2}</b>
+              </div>
+            </div>
+            <button onClick={() => navigate('/order')}>PROCEED TO CHECKOUT</button>
+          </div>
+
+        <div className="cart-promocode">
+          <div>
+            <p>If you have a promo tag, Enter it here</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder='promo code'/>
+              <button type="submit">submit</button>
+            </div>
+          </div>
+        </div>
+
+        </div>
+
     </div>
   )
 }
